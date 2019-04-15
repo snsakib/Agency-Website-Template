c573294a00333a4528670d7ab2c395f6037fe7c1
Syed Nazmus Sakib <nazmus.sakib678@gmail.com>
2019-04-15 10:54:09 +0600

    chore: update deploy.sh to automatically deploy to github-pages

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,13 @@
 echo "Starting script..."
 
 # Production build
-#echo "Building production files..."
-#ng build --prod --output-path docs --base-href /Agency-Website-Template/
+echo "Building production files..."
+ng build --prod --output-path docs --base-href /agency-landing-page/
 
 # Creating 404.html
-#cd docs
-#cp index.html 404.html
+cd docs
+sed -i "s/\(C:\/Program\sFiles\/Git\/agency-landing-page\/\)/\/agency-landing-page\//" index.html
+cp index.html 404.html
 
 # Publishing to GitHub
 echo 'Publishing to GitHub'
