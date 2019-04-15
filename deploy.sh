15b2d237b8bc7759603e10be8b296ea6c4779abf
Syed Nazmus Sakib <nazmus.sakib678@gmail.com>
2019-04-15 09:04:12 +0600

    chore: setup github-pages

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+echo "Starting script..."
+
 # Production build
+#echo "Building production files..."
 #ng build --prod --output-path docs --base-href /Agency-Website-Template/
 
 # Creating 404.html
 #cd docs
 #cp index.html 404.html
 
-# Git commands
+# Publishing to GitHub
+echo 'Publishing to GitHub'
 git add --all
 read -r -p "Commit Message: " CM
 git commit -m "$CM"
