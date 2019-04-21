a11f08002a210b3368a673c694a27e2f59082a2e
Syed Nazmus Sakib <nazmus.sakib678@gmail.com>
2019-04-21 16:07:07 +0600

    refactor: add code to dynamically render works in portafolio component]

--- src/app/portafolio/portafolio.component.ts
+++ src/app/portafolio/portafolio.component.ts
@@ -6,10 +6,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./portafolio.component.scss']
 })
 export class PortafolioComponent implements OnInit {
+  portfolios = [
+    {
+      category: 'Categoría',
+      imgSrc: 'assets/images/portafolio/case-1.png',
+      imgAlt: '#',
+      title: 'Nombre del caso de éxito 1',
+      content:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ullamcorper libero et tempus bibendum.'
+    },
+    {
+      category: 'Categoría',
+      imgSrc: 'assets/images/portafolio/case-2.png',
+      imgAlt: '#',
+      title: 'Nombre del caso de éxito 2',
+      content:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ullamcorper libero et tempus bibendum.'
+    },
+    {
+      category: 'Categoría',
+      imgSrc: 'assets/images/portafolio/case-3.png',
+      imgAlt: '#',
+      title: 'Nombre del caso de éxito 3',
+      content:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ullamcorper libero et tempus bibendum.'
+    },
+    {
+      category: 'Categoría',
+      imgSrc: 'assets/images/portafolio/case-4.png',
+      imgAlt: '#',
+      title: 'Nombre del caso de éxito 4',
+      content:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ullamcorper libero et tempus bibendum.'
+    },
+    {
+      category: 'Categoría',
+      imgSrc: 'assets/images/portafolio/case-5.png',
+      imgAlt: '#',
+      title: 'Nombre del caso de éxito 5',
+      content:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ullamcorper libero et tempus bibendum.'
+    },
+    {
+      category: 'Categoría',
+      imgSrc: 'assets/images/portafolio/case-6.png',
+      imgAlt: '#',
+      title: 'Nombre del caso de éxito 6',
+      content:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ullamcorper libero et tempus bibendum.'
+    }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
