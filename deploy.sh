f8dc7c0ced09972bab695b7140bd8d4cc02780a6
Syed Nazmus Sakib <nazmus.sakib678@gmail.com>
2019-04-23 19:14:26 +0600

    fix: fix bugs regarding responsiveness

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,14 @@
 
 echo "Starting script..."
 
+read -r -p "Enter commit message: " CM
+
 # Production build
 echo "Building production files..."
 ng build --prod --output-path docs --base-href /agency-landing-page/
 
 # Creating 404.html
+echo "Creating 404.html"
 cd docs
 sed -i "s/\(C:\/Program\sFiles\/Git\/agency-landing-page\/\)/\/agency-landing-page\//" index.html
 cp index.html 404.html
@@ -14,6 +17,5 @@ cp index.html 404.html
 # Publishing to GitHub
 echo 'Publishing to GitHub'
 git add --all
-read -r -p "Commit Message: " CM
 git commit -m "$CM"
 git push
