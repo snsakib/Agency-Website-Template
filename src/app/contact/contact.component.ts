dc034e32db0ccbbd79c13a627c98b837823cb989
Syed Nazmus Sakib <nazmus.sakib678@gmail.com>
2019-05-09 01:45:37 +0600

    refactor: remove ngOnInit from contact component class

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
@@ -6,20 +6,16 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent implements OnInit {
-  contactForm: FormGroup;
+export class ContactComponent {
+  contactForm: FormGroup = this.fb.group({
+    name: '',
+    email: '',
+    mobile: '',
+    message: '',
+    terms: true
+  });
 
   sendMessage() {}
 
   constructor(private fb: FormBuilder) {}
-
-  ngOnInit() {
-    this.contactForm = this.fb.group({
-      name: '',
-      email: '',
-      mobile: '',
-      message: '',
-      terms: true
-    });
-  }
 }
