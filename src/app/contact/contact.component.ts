bd6fbd9439e89fb8ea2aedf5baae0d023a92d20b
Syed Nazmus Sakib <nazmus.sakib678@gmail.com>
2019-05-28 08:58:26 +0600

    style: change form field color

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
@@ -6,7 +6,11 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent {
+export class ContactComponent implements AfterViewInit {
+  private formInputUnderlines: HTMLCollectionOf<HTMLDivElement>;
+  private formInputLabels: HTMLCollectionOf<HTMLLabelElement>;
+  private formFieldRipples: HTMLCollectionOf<HTMLSpanElement>;
+
   contactForm: FormGroup = this.fb.group({
     name: '',
     email: '',
@@ -17,5 +21,38 @@ export class ContactComponent {
 
   sendMessage() {}
 
+  changeFormUnderlineColor() {
+    this.formInputUnderlines = document.getElementsByClassName(
+      'mat-form-field-underline'
+    ) as HTMLCollectionOf<HTMLDivElement>;
+    for (const input of Array.from(this.formInputUnderlines)) {
+      input.style.backgroundColor = '#bfbfbf';
+    }
+  }
+
+  changeFormLabelColor() {
+    this.formInputLabels = document.getElementsByClassName(
+      'mat-form-field-label'
+    ) as HTMLCollectionOf<HTMLLabelElement>;
+    for (const input of Array.from(this.formInputLabels)) {
+      input.style.color = '#bfbfbf';
+    }
+  }
+
+  changeRippleColor() {
+    this.formFieldRipples = document.getElementsByClassName(
+      'mat-form-field-ripple'
+    ) as HTMLCollectionOf<HTMLSpanElement>;
+    for (const input of Array.from(this.formFieldRipples)) {
+      input.style.backgroundColor = 'orange';
+    }
+  }
+
   constructor(private fb: FormBuilder) {}
+
+  ngAfterViewInit() {
+    this.changeFormUnderlineColor();
+    this.changeFormLabelColor();
+    this.changeRippleColor();
+  }
 }
