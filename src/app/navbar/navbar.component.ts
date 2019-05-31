dbf02e8147bee93f8989935882fee5fe506427c9
Syed Nazmus Sakib <nazmus.sakib678@gmail.com>
2019-06-01 02:38:26 +0600

    refactor: remove unneccessary comments

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, AfterViewInit, HostListener } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit, AfterViewInit {
+export class NavbarComponent implements OnInit {
   menuOptions = ['Home', 'La Agencia', 'Servicios', 'Portafolio', 'Blog'];
   navLinks: HTMLCollectionOf<Element>;
   // If the width of screen is equals or greather than 1024px, the value of the property becomes true.
@@ -21,38 +21,9 @@ export class NavbarComponent implements OnInit, AfterViewInit {
     }
   }
 
-  // Adds 'active' class to the clicked link
-  // addActiveClass(event: Event) {
-  //   (event.target as HTMLAnchorElement).classList.add('active');
-  // }
-
-  // Removes 'active' class from the current active link
-  // removeActiveClass() {
-  //   for (const navItem of this.navItems) {
-  //     if (navItem.classList.contains('active')) {
-  //       navItem.classList.remove('active');
-  //     }
-  //   }
-  // }
-
-  // Adds click events to all of the links
-  // addClickEvents() {
-  //   for (const navLink of this.navLinks) {
-  //     navLink.addEventListener('click', event => {
-  //       this.removeActiveClass();
-  //       this.addActiveClass(event);
-  //     });
-  //   }
-  // }
-
   constructor() {}
 
   ngOnInit() {
     this.onResize();
-  }
-
-  ngAfterViewInit() {
-    // this.navLinks = document.getElementsByClassName('nav-link');
-    // this.addClickEvents();
   }
 }
